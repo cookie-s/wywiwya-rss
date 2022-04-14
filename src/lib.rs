c7223284414564a7e402d249c13060c530d8214a
cookie-s <kcz@kcz.sh>
2022-04-14 01:33:25 -0400

    init

---
+++ src/lib.rs
@@ -0,0 +1,37 @@
+use serde_json::json;
+use worker::*;
+
+mod utils;
+
+fn log_request(req: &Request) {
+    console_log!(
+        "{} - [{}], located at: {:?}, within: {}",
+        Date::now().to_string(),
+        req.path(),
+        req.cf().coordinates().unwrap_or_default(),
+        req.cf().region().unwrap_or("unknown region".into())
+    );
+}
+
+#[event(fetch)]
+pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
+    log_request(&req);
+
+    utils::set_panic_hook();
+
+    let router = Router::new();
+
+    // Add as many routes as your Worker needs! Each route will get a `Request` for handling HTTP
+    // functionality and a `RouteContext` which you can use to  and get route parameters and
+    // Environment bindings like KV Stores, Durable Objects, Secrets, and Variables.
+    router
+        .get_async("/users/:user_id", |mut _req, ctx| async move {
+            if let Some(user_id) = ctx.param("user_id") {
+                return Response::from_json(&json!({ user_id: user_id }));
+            }
+
+            Response::error("Bad Request", 400)
+        })
+        .run(req, env)
+        .await
+}
